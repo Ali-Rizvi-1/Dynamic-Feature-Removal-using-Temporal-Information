4d494bd67ea0f7663958cb1ebc0a02de9cfd768f
Syed Muhammad Ali Rizvi <63605517+Ali-Rizvi-1@users.noreply.github.com>
2021-12-22 10:33:03 +0500

    update

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import cv2
 
-# Read the query image as query_img
-# and train image This query image
-# is what you need to find in train image
-# Save it in the same directory
-# with the name image.jpg 
+# Read the calculator frame1 as query_img and frame 4 as train image 
+# The features in query image is what you need to find in train image
+
 query_img = cv2.imread('calculator_frame1.jpg')
 train_img = cv2.imread('calculator_frame4.jpg')
   
 # Convert it to grayscale
-query_img_bw = cv2.cvtColor(query_img,cv2.COLOR_BGR2GRAY)
-train_img_bw = cv2.cvtColor(train_img, cv2.COLOR_BGR2GRAY)
+query_img_gray = cv2.cvtColor(query_img,cv2.COLOR_BGR2GRAY)
+train_img_gray = cv2.cvtColor(train_img, cv2.COLOR_BGR2GRAY)
   
 # Initialize the ORB detector algorithm
-orb = cv2.ORB_create(nfeatures=5000)
+orb = cv2.ORB_create(nfeatures=2000)
   
-# Now detect the keypoints and compute
-# the descriptors for the query image
+# Now detect the keypoints and compute the descriptors for the query image
 # and train image
-#queryKeypoints, queryDescriptors = orb.detectAndCompute(query_img_bw,None)
-#trainKeypoints, trainDescriptors = orb.detectAndCompute(train_img_bw,None)
 
-queryKeypoints, queryDescriptors = orb.detectAndCompute(query_img_bw,None)
-trainKeypoints, trainDescriptors = orb.detectAndCompute(train_img_bw,None)
+queryKeypoints, queryDescriptors = orb.detectAndCompute(query_img_gray,None)
+trainKeypoints, trainDescriptors = orb.detectAndCompute(train_img_gray,None)
+
+cv2.imshow("Image", train_img_gray)
+cv2.waitKey(0)
